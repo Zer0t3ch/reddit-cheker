cd4345c33b278b5df17dfa1e2458ca3e4762c0e9
Zachary Hill <zer0t3ch@live.com>
2014-11-13 10:01:17 -0600

    Added credntial loading from a file

--- reddit-check.py
+++ reddit-check.py
@@ -5,12 +5,18 @@ import os
 
 version[0] = 0
 version[1] = 1
+
 usr_agent = "redditChecker/{0}.{1} by zer0t3ch".format(version[0], version[1])
-reddit = praw.Reddit(user_agent=usr_agent)
+r = praw.Reddit(user_agent=usr_agent)
 
 com = sys.argv[1]
+auth_file = "./reddit-auth.creds"
+creds = [ ]
+with open(auth_file, 'r') as fil:
+	for line in xrange(2):
+		creds[line] = fil[line].strip()
+r.login(creds[0], creds[1])
 
-# TODO: Add credential loading from a file
 # TODO: Add message checker command
 # TODO: Add unknown command output
 
