519028590b0ba8d042324e6cd35309efa3cf6000
Zachary Hill <zer0t3ch97@gmail.com>
2014-11-14 08:23:17 -0600

    Various fixes

--- reddit-check.py
+++ reddit-check.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python
+import traceback as tb
 import praw
 import sys
 import os
 
-version[0] = 0
-version[1] = 1
+version = [ 0, 1 ]
 
 usr_agent = "redditChecker/{0}.{1} by zer0t3ch".format(version[0], version[1])
 r = praw.Reddit(user_agent=usr_agent)
 
-com = sys.argv[1]
+if len(sys.argv) > 1:
+	com = sys.argv[1]
+else:
+	print("Missing a command")
+	sys.exit(1)
+
 auth_file = "./reddit-auth.creds"
 creds = [ ]
 with open(auth_file, 'r') as fil:
-	for line in xrange(2):
-		creds[line] = fil[line].strip()
+	for l in fil:
+		creds.append(l.strip())
 	fil.close()
 r.login(creds[0], creds[1])
 
@@ -23,17 +28,26 @@ r.login(creds[0], creds[1])
 ####################################################
 ####################################################
 
+def get_unread():
+	msgs = [ ]
+	unread_messages = r.get_unread()
+	for msg in unread_messages:
+		msgs.append(msg)
+	return "You have {0} unread messages".format(len(msgs))
+
+##########################
+
 commands = {
 	'unread' : get_unread
 }
 
-##########################
-
-def get_unread():
-	n = len(r.get_unread())
-	return "You have {0} unread messages".format(n)
-
 ####################################################
 ####################################################
 
-commands[com]()
+ret = ""
+try:
+	ret = commands[com]()
+except Exception,e:
+	ret = e
+
+print(ret)
